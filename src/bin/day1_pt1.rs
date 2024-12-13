fcf5342bae8d0ed56b094275550552d5165fc5c5
Emily Gray <emily.gray@twosixtech.com>
2024-12-13 09:52:16 -0500

    making it cargo

---
+++ src/bin/day1_pt1.rs
@@ -0,0 +1,31 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+// file I/O - read in file
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+
+
+
+fn main () {
+
+    let mut first_list = Vec::new();
+    let mut sec_list = Vec::new();
+
+    if let Ok(lines) = read_lines("./day1_input.txt") {
+        println!("reading file");
+        for line in lines.flatten() {
+            let temp: Vec<&str> = line.split(' ').collect();
+            first_list.push(temp.first().unwrap().parse::<u32>().unwrap());
+            sec_list.push(temp.last().unwrap().parse::<u32>().unwrap());
+        }
+    }
+    println!("List len: {}",first_list.len());
+
+}
