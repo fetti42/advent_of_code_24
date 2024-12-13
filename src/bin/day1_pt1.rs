39e17f21d2b2f4ec082d714cda14bd7fca49d524
Emily Gray <emily.gray@twosixtech.com>
2024-12-13 10:12:02 -0500

    day 1 part 1 works

--- src/bin/day1_pt1.rs
+++ src/bin/day1_pt1.rs
@@ -19,18 +19,30 @@ fn main () {
     let mut first_list = Vec::new();
     let mut sec_list = Vec::new();
 
+    // read in numbers
     if let Ok(lines) = read_lines("../inputs/day1_input.txt") {
         println!("reading file");
         for line in lines.flatten() {
             let temp: Vec<&str> = line.split(' ').collect();
-            first_list.push(temp.first().unwrap().parse::<u32>().unwrap());
-            sec_list.push(temp.last().unwrap().parse::<u32>().unwrap());
+            first_list.push(temp.first().unwrap().parse::<i32>().unwrap());
+            sec_list.push(temp.last().unwrap().parse::<i32>().unwrap());
         }
     }
+
+    //sort lists
     first_list.sort();
     sec_list.sort();
 
-    let foo = min(sec_list.len(), 5);
-    println!("{:?}",&sec_list[..foo]);
+    //let foo = min(sec_list.len(), 5);
+    //println!("{:?}",&sec_list[..foo]);
+
+    let mut running_sum = 0;
+
+    for i in 0..first_list.len() {
+        let diff = (first_list[i] - sec_list[i]).abs();
+        running_sum += diff;
+    }
+
+    println!("Differences: {}", running_sum);
 
 }
