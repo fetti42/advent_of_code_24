cc97a82b720f0b996b560c78d7ee5ca538448e70
Emily Gray <emily.gray@twosixtech.com>
2024-12-13 13:52:42 -0500

    day 2 part 1 works

--- src/bin/day2_pt1.rs
+++ src/bin/day2_pt1.rs
@@ -11,13 +11,50 @@ where P: AsRef<Path>, {
 }
 
 
-
 fn main () {
 
+    let mut safe_count = 0;
+    let mut reports: Vec<Vec<i32>> = Vec::new();
+
     // read in numbers
-    if let Ok(lines) = read_lines("../inputs/day1_input.txt") {
+    if let Ok(lines) = read_lines("../inputs/day2_input.txt") {
         println!("reading file");
         for line in lines.flatten() {
+            let foo: Vec<i32> = line.split(' ').map(|x| x.parse::<i32>().unwrap()).collect();
+            reports.push(foo);
+        }
+
+        for report in reports {
+            let mut foo = report.clone();
+            foo.sort();
+            if foo != report {
+                foo.reverse();
+                if foo != report {
+                    //println!{"{:?}", report};
+                    continue;
+                }
+            }
+
+            let mut curr_val = &foo[0];
+            let mut bad = false;
+
+            for val in &foo[1..] {
+                if (curr_val - val).abs() < 1 {
+                    bad = true;
+                    break;
+                }
+                if (curr_val - val).abs() > 3 {
+                    bad = true;
+                    break;
+                }
+                curr_val = val;
+            }
+            if bad {
+                continue;
+            }
+            safe_count += 1;
         }
     }
+
+    println!("Safe reports: {}", safe_count);
 }
