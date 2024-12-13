388bfaade7b96e1ec1f9ba77a0e942a626f981a4
Emily Gray <emily.gray@twosixtech.com>
2024-12-13 17:15:59 -0500

    this one sucks and is awful. but it works now.

--- src/bin/day3_pt2.rs
+++ src/bin/day3_pt2.rs
@@ -16,25 +16,32 @@ fn main () {
 
     let mut running_sum = 0;
     // read input
-    let dont_re = Regex::new(r"(?<invalid>don't\(\).*?)do\(\)").unwrap();
+    let dont_re = Regex::new(r"don't\(\).*?(?<end>(do\(\)|don't\(\)|$))").unwrap();
     let re = Regex::new(r"mul\((?<num1>\d\d?\d?),(?<num2>\d\d?\d?)\)").unwrap();
+    let mut all_text  = "do()".to_string();
 
     if let Ok(lines) = read_lines("../inputs/day3_input.txt") {
         println!("reading file");
-
         for line in lines.flatten() {
-            let foo = dont_re.replace_all(&line,"");
-            let muls: Vec<(i32, i32)> = re.captures_iter(&foo).map(|caps| {
-                let num_1 = caps.name("num1").unwrap().as_str().parse::<i32>().unwrap();
-                let num_2 = caps.name("num2").unwrap().as_str().parse::<i32>().unwrap(); 
-                (num_1, num_2)
-            }).collect();
-            println!("num 1: {}, num 2: {}", muls[0].0, muls[0].1);
+            all_text.push_str(&line);
+        } // concat all the input because the don'ts carry over to the next line
 
-            for each in muls {
-                running_sum += each.0 * each.1;
-            }
+        let foo = dont_re.replace_all(&all_text,"$end");
+        let foo = dont_re.replace_all(&foo,"$end");
+        let foo = dont_re.replace_all(&foo,"$end");
+        let foo = dont_re.replace_all(&foo,"$end"); // stops changing after this
 
+        let muls: Vec<(i32, i32)> = re.captures_iter(&foo).map(|caps| {
+            let num_1 = caps.name("num1").unwrap().as_str().parse::<i32>().unwrap();
+            let num_2 = caps.name("num2").unwrap().as_str().parse::<i32>().unwrap(); 
+            (num_1, num_2)
+        }).collect();
+        //for each in &muls {
+        //    println!("num 1: {}, num 2: {}", each.0, each.1);
+        //}
+
+        for each in muls {
+            running_sum += each.0 * each.1;
         }
         println!("Sum is: {}", running_sum);
     }
