f1562fa4dc18b2ff81e77aae5f20b405885a9fca
Emily Gray <emily.gray@twosixtech.com>
2024-12-13 16:23:48 -0500

    removing anything between don't and do, doesn't work

--- src/bin/day3_pt2.rs
+++ src/bin/day3_pt2.rs
@@ -16,13 +16,15 @@ fn main () {
 
     let mut running_sum = 0;
     // read input
+    let dont_re = Regex::new(r"(?<invalid>don't\(\).*?)do\(\)").unwrap();
     let re = Regex::new(r"mul\((?<num1>\d\d?\d?),(?<num2>\d\d?\d?)\)").unwrap();
 
     if let Ok(lines) = read_lines("../inputs/day3_input.txt") {
         println!("reading file");
 
         for line in lines.flatten() {
-            let muls: Vec<(i32, i32)> = re.captures_iter(&line).map(|caps| {
+            let foo = dont_re.replace_all(&line,"");
+            let muls: Vec<(i32, i32)> = re.captures_iter(&foo).map(|caps| {
                 let num_1 = caps.name("num1").unwrap().as_str().parse::<i32>().unwrap();
                 let num_2 = caps.name("num2").unwrap().as_str().parse::<i32>().unwrap(); 
                 (num_1, num_2)
